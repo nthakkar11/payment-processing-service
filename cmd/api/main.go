d76292dafee7b88a49d49e49241372fe7d6df49e
Nikhil Thakkar <npsedani@gmail.com>
2026-01-17 15:05:36 +0000

    Add basic HTTP server with health endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,25 @@
-		
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Server starting on :8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nthakkar11/payment-processing-service
+
+go 1.25.4
